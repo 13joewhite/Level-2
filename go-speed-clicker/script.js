30ecec740d9b4aab2aeeb3e3570c1760a8dfd078
Joseph White <josephwhite@Josephs-MacBook-Pro.local>
2021-01-23 17:03:03 -0700

    Finished go speed clicker and mario pest control.

--- go-speed-clicker/script.js
+++ go-speed-clicker/script.js
@@ -1,1 +1,36 @@
- 
+
+const numberOfClicks = document.getElementById(`button`)
+
+
+let click = 0
+console.log(typeof localStorage.getItem(`clicks`) )
+if(localStorage.getItem(`clicks`) == 0) {
+    click = 0
+} else {
+    click = Number(localStorage.getItem(`clicks`))
+}
+
+numberOfClicks.addEventListener(`click`, (event) => {
+    
+    click += 1
+    
+    localStorage.setItem(`clicks`, click)
+
+
+        const h1 = document.createElement('h1')
+
+        h1.textContent = click
+
+        document.getElementsByTagName("body")[0].append(h1)
+
+
+       console.log(localStorage.getItem(`clicks`))
+
+
+})
+
+
+
+
+
+
--- mario-pest-control/script.js
+++ mario-pest-control/script.js
@@ -6,7 +6,7 @@
      const numberOfGoombas = form.goombasInput.value 
      const priceForGoombas = numberOfGoombas * 5
      const goomba = document.createElement(`h2`)
-     goomba.textContent = priceForGoombas
+     goomba.textContent = `Mario caught ${numberOfGoombas} Goombas. That will be ${priceForGoombas} coins`
      console.log(goomba)
      document.getElementsByTagName(`form`)[0].append(goomba)
 
@@ -15,14 +15,14 @@
      const numberOfBobombs = form.bobombsInput.value 
      const priceForBobombs = numberOfBobombs * 7
      const bobombs = document.createElement(`h2`)
-     bobombs.textContent = priceForBobombs
+     bobombs.textContent = `Mario caught ${numberOfBobombs} Bob-ombs. That will be ${priceForBobombs} coins`
      console.log(bobombs)
      document.getElementsByTagName(`form`)[0].append(bobombs)
 
      const numberOfCheeps = form.cheepsInput.value 
      const priceForCheeps = numberOfCheeps * 11
      const cheeps = document.createElement(`h2`)
-     cheeps.textContent = priceForCheeps
+     cheeps.textContent = `Mario caught ${numberOfCheeps} Cheep-Cheeps. That will be ${priceForCheeps} coins`
      console.log(cheeps)
      document.getElementsByTagName(`form`)[0].append(cheeps)
  })
